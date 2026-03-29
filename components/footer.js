fcdca3225df88a990c55ee62a65f0773ad9fd074
laurazaugg <laura.zaugg@bluewin.ch>
2026-03-29 15:29:11 +0200

    Refactor Footer: Update link paths (same as header)

--- components/footer.js
+++ components/footer.js
@@ -12,10 +12,12 @@ class WebComponentFooter extends HTMLElement {
     }
 
     render() {
+        const ROOT = "/" + window.location.pathname.split("/")[1] + "/";
+
         this.shadowRoot.innerHTML =
             `
-                <link rel="stylesheet" href="../css/style.css">
-                <link rel="stylesheet" href="../css/footer.css">
+                <link rel="stylesheet" href="${ROOT}css/style.css">
+                <link rel="stylesheet" href="${ROOT}css/footer.css">
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
                 
                 <footer class="footerContainer">
